b05af20ca8f7418831db23e59ce532870f90352b
hhFate <243144454@qq.com>
2016-08-17 11:23:46 +0800

    添加极验验证二次验证工具

---
+++ fate-utils/src/main/java/cn/reinforce/utils/GeetestUtil.java
@@ -0,0 +1,42 @@
+package cn.reinforce.utils;
+
+import cn.reinforce.utils.entity.GeetestLib;
+import org.apache.log4j.Logger;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 极验验证的二次验证
+ * Created by Fate on 2016/8/17.
+ */
+public class GeetestUtil {
+
+    private static Logger LOG = Logger.getLogger(GeetestUtil.class);
+
+    public static int validate(String captchaId, String privateKey, HttpServletRequest request){
+
+        GeetestLib gtSdk = new GeetestLib(captchaId, privateKey);
+
+        String challenge = request.getParameter(GeetestLib.fn_geetest_challenge);
+        String validate = request.getParameter(GeetestLib.fn_geetest_validate);
+        String seccode = request.getParameter(GeetestLib.fn_geetest_seccode);
+
+        //从session中获取gt-server状态
+        int gt_server_status_code = (Integer) request.getSession().getAttribute(gtSdk.gtServerStatusSessionKey);
+
+        //从session中获取userid
+        String userid = (String)request.getSession().getAttribute("userid");
+
+        int gtResult = 0;
+
+        if (gt_server_status_code == 1) {
+            //gt-server正常，向gt-server进行二次验证
+            gtResult = gtSdk.enhencedValidateRequest(challenge, validate, seccode, userid);
+        } else {
+            // gt-server非正常情况下，进行failback模式验证
+            LOG.info("failback:use your own server captcha validate");
+            gtResult = gtSdk.failbackValidateRequest(challenge, validate, seccode);
+        }
+        return gtResult;
+    }
+}
